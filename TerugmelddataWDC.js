7f73f9a3714fe93b5a649ee84524202cae74311a
Leon Hertgers <leonhertgers@live.nl>
2019-05-14 09:52:36 +0200

    Update data.json

--- TerugmelddataWDC.js
+++ TerugmelddataWDC.js
@@ -27,7 +27,7 @@
 
 	//download the data
 	myConnector.getData = function(table, doneCallback) {
-		$.getJSON("https://api.acceptatie.kadaster.nl/tms/v1/terugmeldingen?apikey=l71c0911dd8fe14be1abba40a2f4ba3e69", function(resp) {
+		$.getJSON("https://github.com/leonhertgers/Live-Data-in-een-dashboard/data.json", function(resp) {
 			var feat = resp.features,
 				tableData = [];
 
