b7c0eb7b10d8e44285209d5d327198bd2f42aba5
Leon Hertgers <leonhertgers@live.nl>
2019-05-15 17:55:13 +0200

    Get data from github

--- TerugmelddataWDC.js
+++ TerugmelddataWDC.js
@@ -27,8 +27,8 @@
 
 	//download the data
 	myConnector.getData = function(table, doneCallback) {
-//		$.getJSON("https://leonhertgers.github.io/live-data-in-een-dashboard/data.json", function(resp) {
-               $.getJSON("https://api.acceptatie.kadaster.nl/tms/v1/terugmeldingen?apikey=l71c0911dd8fe14be1abba40a2f4ba3e69", function(resp) {
+		$.getJSON("https://leonhertgers.github.io/live-data-in-een-dashboard/data.json", function(resp) {
+//               $.getJSON("https://api.acceptatie.kadaster.nl/tms/v1/terugmeldingen?apikey=l71c0911dd8fe14be1abba40a2f4ba3e69", function(resp) {
 			var feat = resp.features,
 				tableData = [];
 
