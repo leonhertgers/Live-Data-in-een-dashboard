571ef45b0531c6de1ef6009e759627a64488ce1f
leonhertgers <leonhertgers@live.nl>
2019-05-17 16:42:15 +0200

    Import reproject for geometry

--- TerugmelddataWDC.js
+++ TerugmelddataWDC.js
@@ -1,6 +1,8 @@
+import reproject from "reproject";
+
 (function () {
 	//Create the connector
-    var myConnector = tableau.makeConnector();
+	var myConnector = tableau.makeConnector();
 
 	myConnector.getSchema = function (schemaCallback) {
 		var cols = [{
@@ -13,6 +15,9 @@
 		}, {
 			id: "status",
 			dataType: tableau.dataTypeEnum.string
+		}, {	id: "geometry",
+			alias: "geographical location",
+			dataType: tableau.dataTypeEnum.geometry
 		}];
 
 		var tableSchema = {
@@ -27,18 +32,21 @@
 
 	//download the data
 	myConnector.getData = function(table, doneCallback) {
-		$.getJSON("https://github.com/leonhertgers/Live-Data-in-een-dashboard/data.json", function(resp) {
+		$.getJSON("https://leonhertgers.github.io/live-data-in-een-dashboard/data.json", function(resp) {
+//               $.getJSON("https://api.acceptatie.kadaster.nl/tms/v1/terugmeldingen?apikey=l71c0911dd8fe14be1abba40a2f4ba3e69", function(resp) {
 			var feat = resp.features,
 				tableData = [];
 
 			// Iterate over the JSON object
 			for (var i = 0, len = feat.length; i < len; i++) {
+
 				tableData.push({
 					"basisregistratie": feat[i].properties.basisregistratie,
 					"bronhoudernaam": feat[i].properties.bronhoudernaam,
-					"status": feat[i].properties.status
+					"status": feat[i].properties.status,
+					"geometry": feat[i].properties.geometry | reproject --use-epsg-io --from=EPSG:28992 --to=EPSG:4326
 
-				});
+			});
 			}
 
 			table.appendRows(tableData);
@@ -46,13 +54,14 @@
 		});
 	};
 
-    tableau.registerConnector(myConnector);
+	tableau.registerConnector(myConnector);
 })();
 
 $(document).ready(function () {
-    $("#submitButton").click(function () {
-        tableau.connectionName = "BAG Terugmelddata";
-        tableau.submit();
-    });
+	$("#submitButton").click(function () {
+		tableau.connectionName = "BAG Terugmelddata";
+		tableau.submit();
+	});
 });
+
 
