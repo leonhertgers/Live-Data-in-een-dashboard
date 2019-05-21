2f7e2f14dffc75c2fef4278be50683adc91e7be7
leonhertgers <leonhertgers@live.nl>
2019-05-21 15:49:05 +0200

    Add geometry

--- TerugmelddataWDC.js
+++ TerugmelddataWDC.js
@@ -1,30 +1,30 @@
 // import {index} from "./index.js";
-// const reproj = require("./index.js")
+ const reproj = require("./index.js").toWgs84()
 
-scriptLoader('./index.js');
-
- function scriptLoader(path, callback)
- {
-	 var script = document.createElement('script');
-	 script.type = "text/javascript";
-	 script.async = true;
-	 script.src = path;
-	 script.onload = function(){
-		 if(typeof(callback) == "function")
-		 {
-			 callback();
-		 }
-	 }
-	 try
-	 {
-		 var scriptOne = document.getElementsByTagName('script')[0];
-		 scriptOne.parentNode.insertBefore(script, scriptOne);
-	 }
-	 catch(e)
-	 {
-		 document.getElementsByTagName("head")[0].appendChild(script);
-	 }
- }
+// scriptLoader('./index.js');
+//
+//  function scriptLoader(path, callback)
+//  {
+// 	 var script = document.createElement('script');
+// 	 script.type = "text/javascript";
+// 	 script.async = true;
+// 	 script.src = path;
+// 	 script.onload = function(){
+// 		 if(typeof(callback) == "function")
+// 		 {
+// 			 callback();
+// 		 }
+// 	 };
+// 	 try
+// 	 {
+// 		 var scriptOne = document.getElementsByTagName('script')[0];
+// 		 scriptOne.parentNode.insertBefore(script, scriptOne);
+// 	 }
+// 	 catch(e)
+// 	 {
+// 		 document.getElementsByTagName("head")[0].appendChild(script);
+// 	 }
+//  }
 
 (function () {
 	//Create the connector
@@ -127,6 +127,7 @@ scriptLoader('./index.js');
 					"status": feat[i].properties.status,
 					//"geometry": RD2WGS84(feat[i].properties.geometry.coordinates[0], feat[i].properties.geometry.coordinates[1])
 					 "geometry": reproj.toWGS84(feat[i].properties.geometry, 'EPSG:28992', epsg)
+
 				});
 			}
 
