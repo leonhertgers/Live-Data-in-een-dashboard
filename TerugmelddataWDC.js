f223cd0132d26703fd3ac97143da8695d60f6480
leonhertgers <leonhertgers@live.nl>
2019-06-11 13:16:24 +0200

    Add new cols

--- TerugmelddataWDC.js
+++ TerugmelddataWDC.js
@@ -2,35 +2,48 @@
 	//Create the connector
 	var myConnector = tableau.makeConnector();
 
+
+	//the fields I surely need are:
+	// - tijdstipStatusWijziging
+	// - tijdstipWijziging
+	// - tijdstipRegistratie
+	// - meldingsnummer
+
 	myConnector.getSchema = function (schemaCallback) {
 		var cols = [{
 			id: "basisregistratie",
 			dataType: tableau.dataTypeEnum.string
-		},  {
+		}, {
 			id: "bronhoudernaam",
 			alias: "bronhoudernaam",
 			dataType: tableau.dataTypeEnum.string
 		}, {
+			id: "bronhoudercode",
+			alias: "bronhoudercode",
+			dataType: tableau.dataTypeEnum.string
+		}, {
+			id: "meldingsnummer",
+			alias: "meldingsnummer",
+			dataType: tableau.dataTypeEnum.string
+		}, {
 			id: "status",
 			dataType: tableau.dataTypeEnum.string
-		}, //{
-		//	id: "omschrijving",
-		//	datatype: tableau.dataTypeEnum.string
-		//},
-			{
-				id: "location",
-				dataType: tableau.dataTypeEnum.geometry
-			}
-			,
-					{
-					id: "tijdstipRegistratie",
-					datatype: tableau.dataTypeEnum.dateTime
-				}
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js" type="text/javascript"></script>
-		];
+		}, {
+			id: "omschrijving",
+			alias: "Description",
+			dataType: tableau.dataTypeEnum.string
+		}, {
+			id: "tijdstipStatusWijziging",
+			alias: "time 1",
+			dataType: tableau.dataTypeEnum.string
+		}, {
+			id: "location",
+			dataType: tableau.dataTypeEnum.geometry
+		}];
 
 		var tableSchema = {
 			id: "Terugmelddata",
+			alias: "Test",
 			columns: cols
 		};
 
@@ -39,36 +52,29 @@
 
 	//download the data
 	myConnector.getData = function(table, doneCallback) {
-			var Basisregistratie_keus = JSON.parse(tableau.connectionData),
-			keuzeString = "starttime=" + Basisregistratie_keus.keuze,
-			apiCall = "https://leonhertgers.github.io/Live-Data-in-een-dashboard/data.json" + keuzeString;
-			$.getJSON(apiCall, function(resp) {
-			
+		$.getJSON("https://stanronzhin.github.io/terugmeld/data.json", function(resp) {
 			var feat = resp.features;
 			tableData = [];
 			proj4.defs("EPSG:28992","+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs");
 
 			// Iterate over the JSON object
 			for (var i = 0, len = feat.length; i < len; i++) {
-					var dateFormat = "DD-MM-YYYY";
-					var tijdstipRegistratie = moment(feat[i].tijdstipRegistratie.value).format(dateFormat);
-//
+
 				var coordRD = feat[i].geometry.coordinates;
 				var coordWGS =  proj4('EPSG:28992', 'WGS84', coordRD);
 				var geoJson = '{"type":"Point","coordinates":' + JSON.stringify(coordWGS) + '}';
 				var wkt_data = new Wkt.Wkt();
 				wkt_data.read(geoJson);
 				console.log(wkt_data);
-
-
 				tableData.push({
 					"basisregistratie": feat[i].properties.basisregistratie,
 					"bronhoudernaam": feat[i].properties.bronhoudernaam,
+					"bronhoudercode": feat[i].properties.bronhoudercode,
+					"meldingsnummer": feat[i].properties.meldingsNummer,
+					"tijdstipStatusWijziging": feat[i].properties.tijdstipStatusWijziging,
 					"status": feat[i].properties.status,
-				//	"omschrijving": feat[i].omschrijving,
+					"omschrijving": feat[i].properties.omschrijving,
 					"location": wkt_data.toJson()
-					,
-					"tijdstipRegistratie": tijdstipRegistratie
 				});
 			}
 			tableau.log(tableData);
@@ -81,28 +87,12 @@
 	tableau.registerConnector(myConnector);
 })();
 
-$(document).ready(function() {
-	$("#submitButton").click(function() {
-		var Basisregistratie_keus = {
-			keuze: $('#Basisregistratie_keuze').val().trim(),
-
-		};
-
-	//	function isValidChoise(dateStr) {
-	//		var d = new Date(dateStr);
-	//		return !isNaN(d.getDate());
-	//	}
-
-	//	if (isValidDate(dateObj.startDate)) {
-			tableau.connectionData = JSON.stringify(Basisregistratie_keus);
-			tableau.connectionName = "BAG terugmelddata";
-			tableau.submit();
-	//	} else {
-	//		$('#errorMsg').html("Enter valid basisregistratie, for example: BAG, BRT");
-	//	}
+$(document).ready(function () {
+	$("#submitButton").click(function () {
+		tableau.connectionName = "BAG Terugmelddata";
+		tableau.submit();
 	});
 });
-
 
 
 
