bec351704782593dd3ca650b442951b5a2e69370
leonhertgers <leonhertgers@live.nl>
2019-06-12 14:30:15 +0200

    add moment.js

--- TerugmelddataWDC.js
+++ TerugmelddataWDC.js
@@ -55,7 +55,7 @@
 
 	//download the data
 	myConnector.getData = function(table, doneCallback) {
-		$.getJSON("https://leonhertgers.github.io/Live-Data-in-een-dashboard/data.json", function(resp) {
+		$.getJSON("https://leonhertgers.github.io/Live-Data-in-een-dashboard/terugmeldingen.json", function(resp) {
 			var feat = resp.features;
 			tableData = [];
 			proj4.defs("EPSG:28992","+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs");
