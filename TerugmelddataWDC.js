6218fe5f56dcb837897e648d6ff378ff358ad7b6
leonhertgers <leonhertgers@live.nl>
2019-06-26 16:53:25 +0200

    add moment.js

--- TerugmelddataWDC.js
+++ TerugmelddataWDC.js
@@ -56,7 +56,7 @@
 
 	//download the data
 	myConnector.getData = function(table, doneCallback) {
-		$.getJSON("https://leonhertgers.github.io/Live-Data-in-een-dashboard/terugmeldingen_update.json", function(resp) {
+		$.getJSON("https://leonhertgers.github.io/Live-Data-in-een-dashboard/terugmeldingen (1).json", function(resp) {
 		//	$.getJSON("https://api.acceptatie.kadaster.nl/tms/v1/terugmeldingen?apikey=l71c0911dd8fe14be1abba40a2f4ba3e69", function(resp) {
 
 				var feat = resp.features;
